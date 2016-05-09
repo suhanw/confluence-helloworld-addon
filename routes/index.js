3226bfd19afb5a17a314e412eb713e56850c7711
Matthew Jensen <mjensen@atlassian.com>
2016-05-09 17:38:48 +1000

    First implementation of the hello world macro

--- routes/index.js
+++ routes/index.js
@@ -16,27 +16,23 @@ module.exports = function (app, addon) {
         });
     });
 
-    // This is an example route that's used by the default "generalPage" module.
-    // Verify that the incoming request is authenticated with Atlassian Connect
-    app.get('/hello-world', addon.authenticate(), function (req, res) {
-            // Rendering a template is easy; the `render()` method takes two params: name of template
-            // and a json object to pass the context in
+    // Render the macro by returning html generated from the hello-world template.
+    // The hello-world template is defined in /views/hello-world.hbs.
+    app.get('/macro', addon.authenticate(), function (req, res) {
             res.render('hello-world', {
                 title: 'Atlassian Connect'
-                //issueId: req.query['issueId']
             });
         }
     );
 
     // Add any additional route handlers you need for views or REST resources here...
 
-
     // load any additional files you have in routes and apply those to the app
     {
         var fs = require('fs');
         var path = require('path');
         var files = fs.readdirSync("routes");
-        for(var index in files) {
+        for (var index in files) {
             var file = files[index];
             if (file === "index.js") continue;
             // skip non-javascript files
