16008da2fe94e96eb3ea8a49c5d7380d1bcf57ec
Matthew Jensen <mjensen@atlassian.com>
2016-09-05 15:36:18 +1000

    Simplified the macro example

--- routes/macro.js
+++ routes/macro.js
@@ -1,11 +1,8 @@
 module.exports = function (app, addon) {
 
-    // Render the macro by returning html generated from the hello-world template.
-    // The hello-world template is defined in /views/hello-world.hbs.
+    // Render the macro by returning a hello world html message
     app.get('/macro', addon.authenticate(), function (req, res) {
-            res.render('hello-world', {
-                title: 'Atlassian Connect'
-            });
+            res.send('<h1>Hello world</h1>');
         }
     );
 
